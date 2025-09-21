d3e2184c531497db92ecf2dbf658fa6810ab2739
vaishu <vaishnavigajjali@gmail.com>
2025-09-21 16:10:22 +0530

    intial commit- project-pulse

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.jsx'),
+      name: 'ProjectPulse',
+      fileName: (format) => `project-pulse.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
